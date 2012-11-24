4d5f1f071fd9d1b61ce0b511ab9e337048f5eb2a
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-11-24 13:25:13 +0100

    Updated header

--- doc_index.pl
+++ doc_index.pl
@@ -3,7 +3,7 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2010, University of Amsterdam
+    Copyright (C): 1985-2012, University of Amsterdam
 			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
