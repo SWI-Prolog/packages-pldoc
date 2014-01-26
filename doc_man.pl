6a5b26647ed85caf55ab1b490090cfd69a62bc31
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-01-26 16:17:06 +0100

    FIXED: prolog:doc_object_summary(section(ID), ...).  Needed for tagged sections on the website.

--- doc_man.pl
+++ doc_man.pl
@@ -1346,6 +1346,9 @@ pldoc_refman(Request) :-
 		 *	    HOOK SEARCH		*
 		 *******************************/
 
+prolog:doc_object_summary(section(ID), Class, File, Summary) :- !,
+	index_manual,
+	man_index(section(_Level, _No, ID, _Path), Summary, File, Class, _Offset).
 prolog:doc_object_summary(Obj, Class, File, Summary) :-
 	index_manual,
 	man_index(Obj, Summary, File, Class, _Offset).
