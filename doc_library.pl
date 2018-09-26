4d80c0f59c3d57a0730a64e6aab9825040ba22db
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-09-26 10:59:27 +0200

    Exclude newly installed latex2html dir from documentation

--- doc_library.pl
+++ doc_library.pl
@@ -97,6 +97,7 @@ blocked('/clpq').
 blocked('/clpr').
 blocked('/pldoc').
 blocked('/ciao').
+blocked('/latex2html').
 blocked('/checkselect.pl').
 blocked('/tabling.pl').                 % deprecated file
 blocked('/checklast.pl').
