33d1c0b16218703ca751164bec0d489b090528e4
Jan Wielemaker <jan@swi-prolog.org>
2023-12-15 15:26:01 +0100

    FIXED: Avoid dependency on library(pce)
    Allows running the documentation tools without library(pce)

--- doc_http.pl
+++ doc_http.pl
@@ -71,6 +71,9 @@
 :- use_module(pldoc(doc_access)).
 :- use_module(pldoc(doc_pack)).
 :- use_module(pldoc(man_index)).
+:- if(exists_source(library(pce_emacs))).
+:- autoload(library(pce_emacs), [start_emacs/0]).
+:- endif.
 
 /** <module> Documentation server
 
@@ -233,13 +236,15 @@ browser_url(Spec, URL) :-
 
 %!  prepare_editor
 %
-%   Start XPCE as edit requests comming from the document server can
-%   only be handled if XPCE is running.
+%   Start XPCE as edit requests coming from the document server can only
+%   be handled if XPCE is running.
 
+:- if(current_predicate(start_emacs/0)).
 prepare_editor :-
     current_prolog_flag(editor, pce_emacs),
     !,
     start_emacs.
+:- endif.
 prepare_editor.
 
 
